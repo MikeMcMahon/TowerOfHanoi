77d212e5bc3caa5a2dc3147417fb6c6209915656
Mike McMahon <mike@mcmahon.com>
2013-12-02 15:55:14 -0800

    Implements the game disk and adds the functionality to add disks to the simulation

--- main.py
+++ main.py
@@ -3,13 +3,17 @@ Author: Mike McMahon
 
 """
 
-import pygame
-import gamefont
 import sys
-from pygame.locals import *
-from gamecolors import *
-from sprite import GameButton
 from decimal import Decimal
+import math
+
+import pygame
+from pygame.locals import *
+
+import gamefont
+from gamecolors import *
+from sprite import GameButton, TowerDisk
+
 
 window = width, height = 800, 600
 screen = pygame.display.set_mode(window)
@@ -33,7 +37,7 @@ tower_platform.rect.y = 500
 fill_gradient(tower_platform.image, GOLDENROD, DARK_GOLDENROD, None, False, False)
 
 tower_size = tower_width, tower_height = [20, 350]
-tower_y = height - tower_height - (100 - tower_platform.rect.height)
+tower_y = tower_platform.rect.y - tower_height
 
 tower_a = pygame.sprite.Sprite()
 tower_a.image = pygame.Surface(tower_size)
@@ -53,8 +57,6 @@ tower_c.rect = tower_c.image.get_rect()
 tower_c.rect.x, tower_c.rect.y = tower_b.rect.x + 233, tower_y
 fill_gradient(tower_c.image, GOLDENROD, DARK_GOLDENROD, None, False)
 
-
-
 rod_sprites = pygame.sprite.OrderedUpdates(
     tower_a,
     tower_b,
@@ -62,11 +64,30 @@ rod_sprites = pygame.sprite.OrderedUpdates(
     tower_platform
 )
 
+disk_sprites = pygame.sprite.OrderedUpdates()
+
+
+def add_disk():
+    if len(disk_sprites) == 0:
+        mod = 1
+    else:
+        mod = math.log(len(disk_sprites) + 1) / len(disk_sprites)
+    disk = TowerDisk((200 * mod))
+    disk.set_pos(
+        (tower_a.rect.x + (tower_a.rect.width / 2)) - (disk.get_rect().width / 2),
+        (tower_a.rect.y + tower_a.rect.height - disk.get_rect().height) - ((disk.get_rect().height * len(disk_sprites)))
+    )
+    disk_sprites.add(
+        disk
+    )
+    return add_disk
+add_disk_button.on_clicked(add_disk)
+
 
 def main():
-    current_time = pygame.time.get_ticks()
     interval = Decimal(1000) / Decimal(32)
-    next_update = pygame.time.get_ticks() + interval
+    current_time = pygame.time.get_ticks()
+    next_update = current_time
 
     solve_button.set_pos(5, 5)
     add_disk_button.set_pos(solve_button.get_pos()[0] + solve_button.get_width() + 5, 5)
@@ -76,20 +97,23 @@ def main():
         for event in pygame.event.get():
             if event.type == QUIT:
                 sys.exit()
+            if event.type == MOUSEBUTTONDOWN:
+                button_sprites.update(pygame.mouse.get_pos(), pygame.mouse.get_pressed())
 
         # Updates handled here - in real timeee
-        button_sprites.update()
+        button_sprites.update(pygame.mouse.get_pos(), (0, 0, 0, 0))
 
         # Rendering logic goes here
-        if current_time - next_update >= interval:
-            fill_gradient(screen, WHITE, LIGHT_BLUE, None, True, False)
+        if current_time >= next_update:
 
-            rod_sprites.draw(screen)
-            button_sprites.draw(screen)
+            disk_sprites.update()
+            next_update += interval
 
-            next_update = pygame.time.get_ticks() + interval
-        else:
-            current_time = pygame.time.get_ticks()
+        fill_gradient(screen, WHITE, LIGHT_BLUE, None, True, False)
+        rod_sprites.draw(screen)
+        disk_sprites.draw(screen)
+        button_sprites.draw(screen)
+        current_time = pygame.time.get_ticks()
         pygame.display.flip()
 
 
