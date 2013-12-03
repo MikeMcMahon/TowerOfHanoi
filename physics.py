e25bcd81a5b46929a014889f804c04e5f949d4e7
Mike McMahon <mike@mcmahon.com>
2013-12-02 23:37:11 -0800

    Removes unused collision_detection method in favor of pygames collidepoint

--- physics.py
+++ physics.py
@@ -2,17 +2,3 @@
 Author: Mike Mcmahon
 
 """
-
-
-def collision_detection(rect, point):
-    """
-        Does a single point lay within the given rectangle
-    """
-    x, y, w, h = rect
-    x2, y2 = point
-
-    if x <= x2 <= x + w:
-        if y <= y2 <= y + h:
-            return True
-
-    return False
