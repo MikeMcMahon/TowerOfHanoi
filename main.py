6e89c05d8ade85f2be6c4823362f456782a1f833
Mike McMahon <mike@mcmahon.com>
2013-12-02 23:39:18 -0800

    Adds more of the main game logic and state machine support

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ import sys
 from decimal import Decimal
 import math
 
-import pygame
 from pygame.locals import *
 
 import gamefont
 from gamecolors import *
+import gamestate
 from sprite import GameButton, TowerDisk
 
 
@@ -21,13 +21,20 @@ screen = pygame.display.set_mode(window)
 default_font, font_renderer = gamefont.init()
 
 solve_button = GameButton(font_renderer, "Solve")
+reset_button = GameButton(font_renderer, "Reset")
 add_disk_button = GameButton(font_renderer, "Add Disk")
 remove_disk_button = GameButton(font_renderer, "Remove Disk")
+
+tower_a_disks = []
+tower_b_disks = []
+tower_c_disks = []
+total_disks = gamefont.create_label(font_renderer, "Total Disks {}  ".format(0))
 
 button_sprites = pygame.sprite.RenderPlain(
     solve_button,
     add_disk_button,
-    remove_disk_button)
+    remove_disk_button,
+    reset_button)
 
 tower_platform = pygame.sprite.Sprite()
 tower_platform.image = pygame.Surface([700, 40])
@@ -67,21 +74,72 @@ rod_sprites = pygame.sprite.OrderedUpdates(
 disk_sprites = pygame.sprite.OrderedUpdates()
 
 
+def reset_total():
+    total_disks.fill((0, 0, 0, 0))
+    total_disks.blit(gamefont.create_label(font_renderer, "Total Disks {}".format(len(disk_sprites))), (0, 0))
+
+
+def remove_disk():
+    if len(disk_sprites) == 0 or game_state.is_solving or game_state.is_dirty:
+        return remove_disk
+
+    tower_a_disks[len(tower_a_disks)-1].kill()
+    tower_a_disks.pop()
+
+    reset_total()
+    return remove_disk
+
+
 def add_disk():
+    if len(disk_sprites) == 13 or game_state.is_solving or game_state.is_dirty:
+        return
+
     if len(disk_sprites) == 0:
         mod = 1
     else:
         mod = math.log(len(disk_sprites) + 1) / len(disk_sprites)
-    disk = TowerDisk((200 * mod))
+
+    disk = TowerDisk(200 * mod, len(tower_a_disks) + 1)
     disk.set_pos(
         (tower_a.rect.x + (tower_a.rect.width / 2)) - (disk.get_rect().width / 2),
         (tower_a.rect.y + tower_a.rect.height - disk.get_rect().height) - ((disk.get_rect().height * len(disk_sprites)))
     )
-    disk_sprites.add(
-        disk
-    )
+    disk_sprites.add(disk)
+    tower_a_disks.append(disk)
+
+    reset_total()
     return add_disk
+
+
+def solve():
+    game_state.is_solving = ~game_state.is_solving
+
+    if game_state.is_solving:
+        solve_button.set_label("Pause")
+    else:
+        solve_button.set_label("Solve")
+
+    game_state.is_dirty = True
+
+    return solve
+
+
+def reset():
+    if not game_state.is_solving:
+        game_state.is_solving = False
+        game_state.is_dirty = False
+
+        # TODO - we need to move the TOWER A B C sprites back to where they go...
+
+    return reset
+
+
 add_disk_button.on_clicked(add_disk)
+remove_disk_button.on_clicked(remove_disk)
+solve_button.on_clicked(solve)
+reset_button.on_clicked(reset)
+
+game_state = gamestate.GameState()
 
 
 def main():
@@ -92,6 +150,9 @@ def main():
     solve_button.set_pos(5, 5)
     add_disk_button.set_pos(solve_button.get_pos()[0] + solve_button.get_width() + 5, 5)
     remove_disk_button.set_pos(add_disk_button.get_pos()[0] + add_disk_button.get_width() + 5, 5)
+    reset_button.set_pos(remove_disk_button.get_pos()[0] + remove_disk_button.get_width() + 5, 5)
+
+    [add_disk() for x in range(3)]
 
     while True:
         for event in pygame.event.get():
@@ -102,6 +163,7 @@ def main():
 
         # Input in reaaal time
         button_sprites.update(pygame.mouse.get_pos(), (0, 0, 0, 0))
+        disk_sprites.update({"mousepos": pygame.mouse.get_pos()})
 
         # Update logic goes here
         if current_time >= next_update:
@@ -115,6 +177,7 @@ def main():
         disk_sprites.draw(screen)
         button_sprites.draw(screen)
         current_time = pygame.time.get_ticks()
+        gamefont.blit_font(total_disks, screen, (5, solve_button.get_height() + 10))
         pygame.display.flip()
 
 
